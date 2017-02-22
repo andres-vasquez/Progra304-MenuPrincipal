17b95a4ea361b5ed7e8418418fdd2a1a1f7624bf
Andres Vasquez <andres.vasquez.a@hotmail.com>
2017-02-22 09:18:12 -0400

    Ejemplo 4. Menu principal completo. Nuevo color

--- app/src/main/java/android/curso/menuprincipal/MenuPrincipal.java
+++ app/src/main/java/android/curso/menuprincipal/MenuPrincipal.java
@@ -12,6 +12,7 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
